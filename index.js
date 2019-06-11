149dcbe7d064768891f3b7f61a89fb21a415f16f
sjwakean <swakeman@ca.rr.com>
2019-06-11 11:14:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 function produceDrivingRange(range) {
-    return function(blockRange) {
+    return function(startBlock, endBlock) {
         
-        var result = (blockRange[1] - blockRange[0]);
+        var result = (parseInt(endBlock) - parseInt(startBlock));
         var difference = (range - result);
-        if ((difference) > 0) {
+        if (result <= range) {
             return `within range by ${result}`;
         } else {
+           
             return `${Math.abs(difference)} blocks out of range`;
             }
     };
+}
+
+function produceTipCalculator() {
+    return function(tip) {
+        return tip * .1;
+    };
+}
+    const tenPercentTip = produceTipCalculator(10);
+
+function createDriver() {
+let DriverId = 0;
+    // return the class
+    return class {
+        constructor(name) {
+        this.name = name;
+        this.id = ++DriverId;
+        }
+    }
 }
