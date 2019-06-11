045079ae2249fc3b435b54c03d1bde63c317d73c
sjwakean <swakeman@ca.rr.com>
2019-06-11 09:40:50 -0700

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ function produceDrivingRange(range) {
     return function(blockRange) {
         
         var result = (blockRange[1] - blockRange[0]);
-        var difference = (result - range);
-        if (result <= range) {
+        var difference = (range - result);
+        if ((difference) > 0) {
             return `within range by ${result}`;
         } else {
-           
-            return `${${Math.abs(difference)} blocks out of range`;
+            return `${Math.abs(difference)} blocks out of range`;
             }
     };
 }
